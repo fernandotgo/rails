ec4735ebfa58f2965225ab50420239c455119593
Fernando Caceres <fernandotgo@gmail.com>
2018-06-30 20:38:08 -0600

    Finally edited the show and update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,8 +13,22 @@ class ArticlesController < ApplicationController
       end
 
   end
+  
+  def edit
+      @article = Article.find(params[:id]) 
+  end
+
   def show
       @article = Article.find(params[:id])
+  end
+  def update
+      @article = Article.find(params[:id])
+      if @article.update(article_params)
+          flash[:notice] = "Articles was updated successfully"
+          redirect_to article_path(@article)
+      else
+          render 'edit'
+      end
   end
 
   private
