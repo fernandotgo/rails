49846202f832bd4a9ca5d841b12d463af6c81715
Fernando Caceres <fernandotgo@gmail.com>
2018-07-06 20:42:52 +0000

    Modificado Create Article

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_06_032304) do
+ActiveRecord::Schema.define(version: 2018_07_06_034100) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2018_07_06_032304) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
+    t.string "usernam"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
