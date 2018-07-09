815c6697032921dbb24d2a2b461e207185366592
Fernando Caceres <fernandotgo@gmail.com>
2018-07-09 20:43:28 +0000

    User validation with articles

--- db/migrate/20180706034100_create_users.rb
+++ db/migrate/20180706034100_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-        t.string :usernam
+        t.string :username
         t.string :email
         t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_06_034100) do
+ActiveRecord::Schema.define(version: 2018_07_09_201056) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "usernam"
+    t.string "username"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
